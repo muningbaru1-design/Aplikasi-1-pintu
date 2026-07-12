2b67fdd1e964b5ed9da405a42fb7204be79cfd3b
muningbaru1-design <muningbaru1@gmail.com>
2026-07-12 18:17:10 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,77 @@
+/* ===========================================
+   DATA MENU APLIKASI 1 PINTU
+=========================================== */
 
+const gridMenu = [
+
+{
+    id:"administrasi",
+    nama:"Buku Administrasi",
+    icon:"bi-book-fill",
+    warna:"#16A34A"
+},
+
+{
+    id:"spj",
+    nama:"Upload SPJ",
+    icon:"bi-cloud-arrow-up-fill",
+    warna:"#2563EB"
+},
+
+{
+    id:"gaji",
+    nama:"Tanda Terima Gaji",
+    icon:"bi-cash-stack",
+    warna:"#F59E0B"
+},
+
+{
+    id:"rumah",
+    nama:"Data Rumah",
+    icon:"bi-house-fill",
+    warna:"#22C55E"
+},
+
+{
+    id:"peta",
+    nama:"Peta Rumah",
+    icon:"bi-geo-alt-fill",
+    warna:"#DC2626"
+},
+
+{
+    id:"undangan",
+    nama:"Kumpulan Undangan",
+    icon:"bi-envelope-fill",
+    warna:"#8B5CF6"
+},
+
+{
+    id:"rapat",
+    nama:"Catat Rapat",
+    icon:"bi-pencil-square",
+    warna:"#14B8A6"
+},
+
+{
+    id:"gotong",
+    nama:"Gotong Royong",
+    icon:"bi-image-fill",
+    warna:"#F97316"
+},
+
+{
+    id:"bumdes",
+    nama:"PADes & BUMDes",
+    icon:"bi-bank",
+    warna:"#0EA5E9"
+},
+
+{
+    id:"setting",
+    nama:"Pengaturan",
+    icon:"bi-gear-fill",
+    warna:"#6B7280"
+}
+
+];
