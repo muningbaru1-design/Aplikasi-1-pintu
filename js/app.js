af3582fc72983b066f581f03e356ec15781b6762
muningbaru1-design <muningbaru1@gmail.com>
2026-07-12 18:18:20 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,71 @@
+/* ===========================================
+   MENU
+=========================================== */
 
+function bukaMenu(menu){
+
+switch(menu){
+
+case "administrasi":
+
+alert("Menu Buku Administrasi");
+
+break;
+
+case "spj":
+
+alert("Menu Upload SPJ");
+
+break;
+
+case "gaji":
+
+alert("Menu Tanda Terima Gaji");
+
+break;
+
+case "rumah":
+
+alert("Menu Data Rumah");
+
+break;
+
+case "peta":
+
+alert("Menu Peta Rumah");
+
+break;
+
+case "undangan":
+
+alert("Menu Kumpulan Undangan");
+
+break;
+
+case "rapat":
+
+alert("Menu Catat Rapat");
+
+break;
+
+case "gotong":
+
+alert("Menu Upload Gotong Royong");
+
+break;
+
+case "bumdes":
+
+alert("Menu PADes & BUMDes");
+
+break;
+
+case "setting":
+
+alert("Menu Pengaturan");
+
+break;
+
+}
+
+}
